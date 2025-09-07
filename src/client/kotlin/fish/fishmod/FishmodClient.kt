240190a704d91411c79ef03934034137e46ee1b8
nojusmama2 <108085921+nojusmama2@users.noreply.github.com>
2025-09-07 10:07:52 +0300

    Add files via upload

---
+++ src/client/kotlin/fish/fishmod/FishmodClient.kt
@@ -0,0 +1,9 @@
+package fish.fishmod
+
+import net.fabricmc.api.ClientModInitializer
+
+object FishmodClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/fish/fishmod/FishmodDataGenerator.kt
@@ -0,0 +1,9 @@
+package fish.fishmod
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object FishmodDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
